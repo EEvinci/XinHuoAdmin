a7c60071688a3e2965afbf85c66b47c9d9ec5039
Evinci <93924805+EEvinci@users.noreply.github.com>
2022-11-30 22:21:29 +0800

    deployed version

--- src/main/java/com/ruoyi/project/system/menu/domain/Menu.java
+++ src/main/java/com/ruoyi/project/system/menu/domain/Menu.java
@@ -8,9 +8,9 @@ import org.apache.commons.lang3.builder.ToStringStyle;
 import com.ruoyi.framework.web.domain.BaseEntity;
 
 /**
- * 菜单权限表 sys_menu
+ *
  * 
- * @author ruoyi
+ * @author kevin
  */
 public class Menu extends BaseEntity
 {
